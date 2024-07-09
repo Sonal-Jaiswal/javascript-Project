66b7aa7307dca8cb560b29c0abc60e3fe2a21052
Sonal Jaiswal <97275122+Sonal-Jaiswal@users.noreply.github.com>
2024-07-09 22:02:38 +0000

    datatypes

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId =144553 // cant be changed
+let accountEmail = "Sonal@gmail.com"
+var accountPassword = "12345"
+
+// let vs var (we should avoid var due to scope)
+
+accountCity = "Jaipur"
+
+console.log(accountId)
+console.table([accountPassword,accountId,accountEmail,accountCity])
